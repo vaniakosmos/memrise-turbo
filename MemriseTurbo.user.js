140fc9dc8126eb9ee85bad28d2572607e0f79569
Andrew Pantyukhin <infofarmer@gmail.com>
2015-06-14 16:09:53 +0300

    0.1.3: injection wrapper seems unnecessary

--- MemriseTurbo.user.js
+++ MemriseTurbo.user.js
@@ -4,44 +4,33 @@
 // @description    Makes Memrise faster
 // @match          http://www.memrise.com/course/*/garden/*
 // @match          http://www.memrise.com/garden/water/*
-// @version        0.1.2
+// @version        0.1.3
 // @updateURL      https://github.com/infofarmer/memrise-turbo/raw/master/MemriseTurbo.user.js
 // @downloadURL    https://github.com/infofarmer/memrise-turbo/raw/master/MemriseTurbo.user.js
 // @grant          none
 // ==/UserScript==
 
-var onLoad = function($) {
+var oldstart = MEMRISE.garden.feedback.start;
+MEMRISE.garden.feedback.start = function (){
+	if (MEMRISE.garden.box.state === 'choosing-mem') {
+		oldstart(1);
+	}else{
+		MEMRISE.garden.box.next_press();
+	}
+};
 
-	var oldstart = MEMRISE.garden.feedback.start;
-	MEMRISE.garden.feedback.start = function (){
-		if (MEMRISE.garden.box.state === 'choosing-mem') {
-			oldstart(1);
-		}else{
-			MEMRISE.garden.box.next_press();
-		}
-	};
-
-	$('body').on('keyup', function(e) {
-		try {
-			if ($(e.target).is('input')) {
-				var g = MEMRISE.garden;
-				var b = g.box;
-				var s = g.scoring.score_response(
-					b.$input.val(),b.thing,b.column_a,b.column_b);
-				if (s === 1) {
-					MEMRISE.garden.box.check();
-				}
+$('body').on('keyup', function(e) {
+	try {
+		if ($(e.target).is('input')) {
+			var g = MEMRISE.garden;
+			var b = g.box;
+			var s = g.scoring.score_response(
+				b.$input.val(),b.thing,b.column_a,b.column_b);
+			if (s === 1) {
+				MEMRISE.garden.box.check();
 			}
-		} catch (err) {
-			console.log('error - falling back to default behavior', err);
 		}
-	});
-};
-
-var injectWithJQ = function(f) {
-	var script = document.createElement('script');
-	script.textContent = '$(' + f.toString() + '($));';
-	document.body.appendChild(script);
-};
-
-injectWithJQ(onLoad);
+	} catch (err) {
+		console.log('error - falling back to default behavior', err);
+	}
+});
