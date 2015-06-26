d7b562b5f2cae9f7c496b60c0e90c681af798442
Andrew Pantyukhin <infofarmer@gmail.com>
2015-06-26 22:39:14 +0300

    tabs expanded

--- MemriseTurbo.user.js
+++ MemriseTurbo.user.js
@@ -13,27 +13,27 @@
 
 var oldstart = MEMRISE.garden.feedback.start;
 MEMRISE.garden.feedback.start = function (){
-	if (MEMRISE.garden.box.state === 'choosing-mem') {
-		oldstart(1);
-	}else{
-		MEMRISE.garden.box.next_press();
-	}
+    if (MEMRISE.garden.box.state === 'choosing-mem') {
+        oldstart(1);
+    }else{
+        MEMRISE.garden.box.next_press();
+    }
 };
 
 $('body').on('keyup', function(e) {
-	try {
-		if ($(e.target).is('input')) {
-			var g = MEMRISE.garden;
-			var b = g.box;
-			var s = g.scoring.score_response(
-				b.$input.val(),b.thing,b.column_a,b.column_b);
-			if (s === 1) {
-				MEMRISE.garden.box.check();
-			}
-		}
-	} catch (err) {
-		console.log('error - falling back to default behavior', err);
-	}
+    try {
+        if ($(e.target).is('input')) {
+            var g = MEMRISE.garden;
+            var b = g.box;
+            var s = g.scoring.score_response(
+                b.$input.val(),b.thing,b.column_a,b.column_b);
+            if (s === 1) {
+                MEMRISE.garden.box.check();
+            }
+        }
+    } catch (err) {
+        console.log('error - falling back to default behavior', err);
+    }
 });
 
 // always let audio play in full
